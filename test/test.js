21b5291911c9ff081544b2a43a4a96a8d2510afa
sukinull <sukinull@gmail.com>
2014-03-21 07:34:33 +0000

    Helper to fetch tiles

--- test/test.js
+++ test/test.js
@@ -1,8 +1,13 @@
+#! /usr/bin/env node
 var assert = require('assert'),
     GSV = require('../lib/gsv.js');
 
-GSV.on('location', function(p, loc) {
-  console.log(p, JSON.stringify(loc));
+GSV.config('dist', '/tmp');
+
+GSV.on('location', function(panoid, loc) {
+  // console.log(panoid, JSON.stringify(loc));
+  if(panoid == loc.panoId) // meaningless, show the panoid should equal loc.panoId
+    console.log(GSV.tile(panoid, 1, 1));
 });
 
 GSV.on('link', function(p, link) {
